89a4a6de6a7b88504a74db0960c791a5c1185815
Nicolas Rigolin <94136312+Lenhador-de-Bonsai@users.noreply.github.com>
2022-01-15 13:13:32 -0300

    Add files via upload

---
+++ src/componentes/Menu.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+	Container
+} from 'reactstrap';
+
+export const Menu = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+    return(
+		<div>
+			<Navbar color="info" dark expand="md" >
+				<Container>
+					<NavbarBrand href="/"> TI Academy </NavbarBrand>
+					<NavbarToggler onClick={function noRefCheck(){}} />
+					<Collapse navbar>
+						<Nav className="me-auto" navbar>
+							<NavItem>
+								<NavLink href="/"> Home </NavLink>
+							</NavItem>
+						</Nav>
+					</Collapse>
+				</Container>
+			</Navbar>
+		</div>
+    );
+};
